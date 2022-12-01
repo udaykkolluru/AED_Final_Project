ee0a260049eb3687db4b5c8e028265ed895d7f6c
uday-kiran-k <113407692+uday-kiran-k@users.noreply.github.com>
2022-12-01 13:03:45 -0500

    merge uday-branch into pg-branch

---
+++ src/main/java/model/EcoSystem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import model.Organisation.Organisation;
+
+/**
+ *
+ * @author charanpatnaik
+ */
+public class EcoSystem extends Organisation{
+    
+}
---
+++ src/main/java/model/Enterprise/EnterpriseDirectory.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.Enterprise;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pawan
+ */
+public class EnterpriseDirectory {
+    private ArrayList<Enterprise> enterpriseList;
+}
