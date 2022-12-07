523b5314155ec840c0e3277e997df36f2587ede5
Charan Patnaik <charanpatnaik@Charans-MacBook-Air.local>
2022-12-07 16:43:34 -0500

    hospital admin workarea UI

--- src/main/java/model/Organization/OrganizationDirectory.java
+++ src/main/java/model/Organization/OrganizationDirectory.java
@@ -24,6 +24,10 @@ public class OrganizationDirectory {
     public void setOrganizationList(ArrayList<Organization> organizationList) {
         this.organizationList = organizationList;
     }
+
+    public void createOrganization(Organization.Type type) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
    
     
