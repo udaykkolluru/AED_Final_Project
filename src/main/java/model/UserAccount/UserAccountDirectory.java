d55422a718a589cbbf4d0923727f55cfac0124e6
Charan Patnaik <charanpatnaik@Charans-MacBook-Air.local>
2022-12-09 20:50:12 -0500

    Manufacturer admin workarea UI

--- src/main/java/model/UserAccount/UserAccountDirectory.java
+++ src/main/java/model/UserAccount/UserAccountDirectory.java
@@ -5,6 +5,8 @@
 package model.UserAccount;
 
 import java.util.ArrayList;
+import model.Employee.Employee;
+import model.Role.Role;
 
 /**
  *
@@ -12,4 +14,16 @@ import java.util.ArrayList;
  */
 public class UserAccountDirectory {
     private ArrayList<UserAccount> userAccountList;
+
+    public Iterable<UserAccount> getUserAccountList() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void createUserAccount(String userName, String password, Employee employee, Role role) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public boolean checkIfUsernameIsUnique(String userName) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
